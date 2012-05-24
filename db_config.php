7bc6451e297cbf0014c65481418d9c3904758c8f
Torin Nguyen <torinnguyen@gmail.com>
2012-05-24 22:16:29 +0800

    Minor correction for db_config

--- db_config.php
+++ db_config.php
@@ -1,12 +1,14 @@
 <?
 /*
 - Created by Torin
-- Database configurations for Cyclone project
+- Database connection utility
 */
 
 require_once("db_config.inc");
 require_once("util_output.php");
 require_once("util_database.php");
+
+global $link;
 
 $link = mysql_connect($DATABASE_HOST,$DATABASE_USER,$DATABASE_PASS) or func_dieWithMessage("Cannot connect to database");
 mysql_select_db($DATABASE_NAME, $link) or func_dieWithMessage("Cannot select database");
